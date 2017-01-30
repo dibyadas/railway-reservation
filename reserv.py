0497ae7b977d1045f94f20827e876297835fbc50
Dibya Das <dibyadascool@gmail.com>
2017-01-30 20:07:16 +0530

    Final

--- reserv.py
+++ reserv.py
@@ -1,8 +1,10 @@
-import pickle
-import sys
 import random
+
+logged_in = False
+uid = 0
+pwd = ''
+
 class train:
-	'''class defining train'''
     def __init__(self,name = '', num = 0, arr_time = '',dep_time = '',src = '' ,des = '',day_of_travel = '',seat_available_in_1AC = 0,seat_available_in_2AC = 0,seat_available_in_SL = 0,fare_1ac = 0, fare_2ac = 0 ,fare_sl = 0):
         self.name = name
         self.num = num
@@ -35,9 +37,10 @@ class train:
 
 
 class ticket:
-	'''class for ticket '''
-	def __init__(self,train,user,ticket_num):
+	def __init__(self,train,user,ticket_num,coach):
 		self.pnr = str(train.num)+str(user.uid)+str(random.randint(100,999))
+		self.train_num = train.num
+		self.coach = coach
 		self.train_name = train.name
 		self.user_name = user.name
 		self.ticket_num = ticket_num
@@ -47,7 +50,6 @@ class ticket:
 
 
 class user:
-	'''class for user'''
 	def __init__(self,uid = 0,name = '',hometown = '',cell_num = '',pwd = ''):
 		self.uid = uid
 		self.name = name
@@ -93,7 +95,7 @@ class acceptors:
 
 	def accept_menu_option():
 		option = input("Enter your option :- ")
-		if option not in ('1','2','3','4','5','6','7'):
+		if option not in ('1','2','3','4','5','6','7','8'):
 			print("Please enter a valid option!")
 			return acceptors.accept_menu_option()
 		else:
@@ -112,7 +114,7 @@ class acceptors:
 		prompt = input("Confirm? (y/n) :-")
 		if prompt not in ('y','n'):
 			print("Please enter proper choice.")
-			acceptors.accept_prompt()
+			return acceptors.accept_prompt()
 		return prompt		
 
 	def accept_ticket_num():
@@ -124,16 +126,20 @@ class acceptors:
 			return acceptors.accept_ticket_num()
 		else:
 			return ticket_num
+	def accept_pnr():
+		pnr = input("Enter your PNR number :- ")
+		if pnr not in ticket_dict:
+			print("Please enter proper PNR number :- ")
+			return acceptors.accept_pnr()
+		else:
+			return pnr
+
 
 
 def book_ticket():
-	uid = acceptors.accept_uid()
-	pwd = acceptors.accept_pwd()
-	if uid in users and users[uid].pwd == pwd:		
-		print("Welcome ",users[uid].name," !")
-	else:
-		print("\n\nNo such user ID / Wrong Password !\n\n")
-		menu()
+	if not logged_in:
+		login('p')
+
 	check_seat_availabilty('p')
 	choice = acceptors.accept_train_number()
 	trains[choice].print_seat_availablity()
@@ -145,7 +151,7 @@ def book_ticket():
 		if prompt == 'y':
 			trains[choice].book_ticket(coach,ticket_num)
 			print("Booking Successful!\n\n")
-			tick = ticket(trains[choice],users[uid],ticket_num)
+			tick = ticket(trains[choice],users[uid],ticket_num,coach)
 			print("Please note PNR number :- ",tick.pnr,"\n\n")
 			menu()
 		else:
@@ -156,27 +162,23 @@ def book_ticket():
 		menu()
 
 def cancel_ticket():
-	pnr = input("Enter the PNR number :- ")
+	pnr = acceptors.accept_pnr()
 	if pnr in ticket_dict:
 		check_pnr(pnr)
-		choice = input("Do you want to cancel(y/n):- ")
-		if choice == 'y':
-			try:
-				uid = int(input("Enter your id :- "))
-			except ValueError:
-				print
-			pwd = input("Enter the password :- ")
-			if uid in users:
-				if users[uid].pwd == pwd:
-					print("\n\nTicket Cancelled.\n\n")
-				else:
-					print("\nEnter the right password\n")
+		print("Cancel the tickets?")
+		prompt = acceptors.accept_prompt()
+		if prompt == 'y':
+			if logged_in:
+				print("Ticket Cancelled.\n")
+				trains[ticket_dict[pnr].train_num].seats[ticket_dict[pnr].coach] += ticket_dict[pnr].ticket_num
+				del ticket_dict[pnr]
 			else:
-				print("\nNo such user.Ticket not cancelled\n")
-				menu()
+				login('p')
+				print("Ticket Cancelled.\n")
+				trains[ticket_dict[pnr].train_num].seats[ticket_dict[pnr].coach] += ticket_dict[pnr].ticket_num
+				del ticket_dict[pnr]
 		else:
 			print("\nTicket not cancelled\n")
-			menu()
 	menu()
 
 
@@ -190,7 +192,7 @@ def check_seat_availabilty(flag = ''):
 			print("Train Name :- ",trains[i].name ," " ,"Number ",trains[i].num ," ","Day of Travel :- ",trains[i].day_of_travel)
 			flag_2 += 1
 	if flag_2 == 0:
-		print("\n\nNo trains found between the stations you entered.\n\n")
+		print("\nNo trains found between the stations you entered.\n")
 		menu()
 	if flag == '':
 		train_num = acceptors.accept_train_number()
@@ -199,16 +201,23 @@ def check_seat_availabilty(flag = ''):
 	else:
 		pass
 
-def check_pnr(pnr_num = ''):
-	if pnr_num == '':
-		pnr_num = input("Enter the PNR number :- ")
-	if pnr_num in ticket_dict:
-		print("User name:- ",ticket_dict[pnr_num].user_name)
-		print("Train name:- ",ticket_dict[pnr_num].train_name)
-		print("No. of Tickets Booked :- ",ticket_dict[pnr_num].ticket_num)
+def check_pnr(pnr = ''):
+	if pnr == '':
+		pnr = acceptors.accept_pnr()
+		print()
+		print("User name:- ",ticket_dict[pnr].user_name)
+		print("Train name:- ",ticket_dict[pnr].train_name)
+		print("Train number:- ",ticket_dict[pnr].train_num," Source:- ",trains[ticket_dict[pnr].train_num].src," Destination:- ",trains[ticket_dict[pnr].train_num].des)
+		print("No. of Tickets Booked :- ",ticket_dict[pnr].ticket_num)
+		print()
+		menu()
 	else:
-		print("\nNo such PNR number exists.\n")
-	menu()
+		print()
+		print("User name:- ",ticket_dict[pnr].user_name)
+		print("Train name:- ",ticket_dict[pnr].train_name)
+		print("Train number:- ",ticket_dict[pnr].train_num," Source:- ",trains[ticket_dict[pnr].train_num].src," Destination:- ",trains[ticket_dict[pnr].train_num].des)
+		print("No. of Tickets Booked :- ",ticket_dict[pnr].ticket_num)
+		print()
 
 def create_new_acc():
 	user_name = input("Enter your user name:- ")
@@ -221,35 +230,47 @@ def create_new_acc():
 	users.update({u.uid : u})
 	menu()
 
-# def check_prev_trans():
-# 	uid = int(input("Enter your id :- "))
-# 	pwd = input("Enter the password :- ")
-# 	if uid in users:
-# 		if users[uid].pwd == pwd: 
+
+
+def login(flag = ''):
+	global uid
+	global pwd
+	uid = acceptors.accept_uid()
+	pwd = acceptors.accept_pwd()
+	if uid in users and users[uid].pwd == pwd:		
+		print("\nWelcome ",users[uid].name," !\n")
+		global logged_in
+		logged_in = True
+	else:
+		print("\nNo such user ID / Wrong Password !\n")
+		return login()
+	if flag == '':
+		menu()
+	else:
+		pass
+
+def check_prev_bookings():
+	if not logged_in:
+		login('p')
+	for i in users[uid].history:
+		check_pnr(i)
+	menu()
+
+def end():
+	print("------------------------------------------------Thank You!-----------------------------------------------------------------------")
+	print("---------------------------------------------------------------------------------------------------------------------------------")
+
 
 
 t1 = train('odisha',12345,'12:34','22:12','ctc','kgp','Wed',30,23,43,2205,320,234)
 t2 = train('howrah',12565,'02:34','23:12','hwr','kol','Mon',33,4,12,3434,435,234)
 t3 = train('bangalore',22353,'11:56','03:12','ctc','ban','Fri',33,24,77,455,325,533)
 trains = {t1.num:t1,t2.num:t2,t3.num:t3}
-# trains.update({int(input("Enter the train number=")) : train(input("Enter the train name="),34353,'12:34','11:52','Wed',11,35,76)})
-# print(train_dict[12565].name)
 u1 = user(1212,'dibya das','cuttack','7478021777','dibyadas')
-users={u1.uid : u1}
+u2 = user(2322,'alex parrish','new york','7873752967','alexparrish')
+users={u1.uid : u1, u2.uid : u2}
 ticket_dict = {}
 
-# train_file = open("t.pkl",'wb')
-# user_file = open("u.pkl",'wb')
-# pickle.dump(trains,train_file)
-# pickle.dump(users,user_file)
-# train_file.close()
-# user_file.close()
-# train_file = open("t.pkl","rb")
-# user_file = open("u.pkl","rb")
-# trains = pickle.load(train_file)
-# users = pickle.load(user_file)
-# train_file.close()
-# user_file.close()
 
 print("--------------------------------------------------Welcome to Railway Reservation Portal----------------------------------------------")
 print("-------------------------------------------------------------------------------------------------------------------------------------")
@@ -262,8 +283,10 @@ def menu():
 	print("3.Check PNR ")
 	print("4.Check seat availibity")
 	print("5.Create new account")
-	print("6.Check previous transaction")
-	print("7.Exit")
+	print("6.Check previous bookings")
+	print("7.Login")
+	print("8.Exit")
+
 	option = acceptors.accept_menu_option()
 	if option == 1:
 		book_ticket()
@@ -275,9 +298,12 @@ def menu():
 		check_seat_availabilty()
 	elif option == 5:
 		create_new_acc()
-
+	elif option == 6:
+		check_prev_bookings()
 	elif option == 7:
-		sys.exit()
+		login()
+	elif option == 8:
+		end()
 
 menu()
 
